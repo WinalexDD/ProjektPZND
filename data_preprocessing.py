8df7e24f9b1e3396d9094fdda0e54fbf258aec30
WinalexDD <146812784+winalexdd@users.noreply.github.com>
2024-01-30 01:41:34 +0100

    Added new param test_size to preprocessing.

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -6,11 +6,12 @@ from sklearn.model_selection import train_test_split
 import dvc.api
 
 #Read params
-params=dvc.api.params_show()
+params = dvc.api.params_show()
 null_threshold = params['Datapick']['null_threshold']
 country_name = params['Datapick']['country_name']
 year = params['Datapick']['year']
-random_state= params['Datapick']['random_state']
+random_state = params['Datapick']['random_state']
+test_size =  params['Datapick']['test_size']
 
 #Getting Data
 dataframe=pd.read_csv(cfg.DATAPATH)
@@ -30,7 +31,7 @@ for name in data_clearing.num_vs_cat(dataframe)[0]:
 #Diving dataset for train and test sets
 X = dataframe.drop(['suicide_number'], axis =1)
 y = dataframe['suicide_number']
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=random_state)
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=test_size, random_state=random_state)
 
 #Saving updating dataset
 np.save(cfg.PROCESSEDDATAPATH, np.array([X_train, X_test, y_train, y_test], dtype=object))
