4a62248c60f30df5b79a46fbc0398af73a0d9764
WinalexDD <146812784+winalexdd@users.noreply.github.com>
2024-01-31 13:06:20 +0100

    Added variable3 in check params part.

--- plotting_data.py
+++ plotting_data.py
@@ -15,7 +15,7 @@ variable3 = params['plotting']['histogram_var']
 bins = params['plotting']['histogram_bins']
 
 #Check if params meet the conditions
-for var in [variable1, variable2]:
+for var in [variable1, variable2, variable3]:
     if not type(var) is str:
         raise TypeError("Only strings are allowed")
     if var not in num_vs_cat(dataframe)[1]:
